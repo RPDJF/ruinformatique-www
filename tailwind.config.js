0a526f3cebeaf4437282008ba10e165e55622ac6
Rui Paulo De Jesus Ferreira <ruipaulo.unify@outlook.fr>
2024-01-13 00:23:11 +0100

    Add current

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html, js}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#192833ce",
+				secondary: "#133d62",
+			},
+		},
+	},
+	plugins: [],
+}
+
